adf8860f3892703992f5e02de57c30abbed3f102
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 15:48:10 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,8 +20,8 @@ def update_status
       }
    }
 
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 
@@ -59,6 +59,8 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
+epic_tragedy[:additional_characters] = []
+epic_tragedy[:additional_characters].push({name: "Prince Escalus"}, {name: "Apothecary"})
 
 
 
